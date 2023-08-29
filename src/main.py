539f035a0990813725323e5c8f29e6d1553eb64a
fsosap <fsosap@eafit.edu.co>
2023-08-29 14:18:56 -0500

    user query feature is fully developed

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import loader       as loader
 import processor    as processor
 import model        as model
 import sql_feeder   as sql_feed
+import db_operations    as db_ops
 
 
 if __name__ == "__main__":
@@ -22,3 +23,11 @@ if __name__ == "__main__":
 
     # Load tables into sqlite db
     sql_feed.orquestrate()
+    
+    while True:
+        print("Provide the query you want to execute:\
+              \n Example: \
+              \n * SELECT AVG(rating) FROM Show\
+              \n * SELECT AVG(rating) FROM Episode\
+              \n * SELECT COUNT(show_id), name FROM GenrexShow INNER JOIN Genre ON Genre.genre_id = GenrexShow.genre_id GROUP BY genre_id")
+        print(db_ops.execute_sql_query(input("Your query: ")))
